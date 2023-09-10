bb7b6d8e9a693602b0b81b68ebc272d0b7ef555b
nohac14 <91922445+nohac14@users.noreply.github.com>
2023-09-10 16:26:48 -0400

    pointers update

--- game.c
+++ game.c
@@ -5,7 +5,6 @@
 int main()
 {
     int PlayerInput;
-    int CPUInput;
 
     srand(time(NULL)); // Moved srand to the main function to seed the random number generator only once.
 
@@ -26,7 +25,7 @@ int main()
 void PlayRPS(int *PlayerInput)
 {
     printf("CHOOSE YOUR FIGHTER!\n|ROCK(1)|PAPER(2)|SCISSORS(3)|\n");
-    *PlayerInput = ChoicePlayer(*PlayerInput); // Pass the address and dereference PlayerInput
+    ChoicePlayer(&PlayerInput); // Pass the address and dereference PlayerInput
     int CPUInput = ChoiceCPU();
     printf("CPU: %d", CPUInput);
     Fight(*PlayerInput, CPUInput);
@@ -34,49 +33,25 @@ void PlayRPS(int *PlayerInput)
 
 int ChoiceCPU()
 {
-    int choice = rand() % 3;
+    int choice = rand() % 3 + 1;
     return choice;
 }
 
-int ChoicePlayer(int playerInput)
+int ChoicePlayer(int **playerInput)
 {
     printf("YOU: ");
-    scanf("%d", &playerInput); // Corrected the scanf format and added the & symbol to read the input into playerInput.
+    scanf("%d", &**playerInput); // Corrected the scanf format and added the & symbol to read the input into playerInput.
     return playerInput;
 }
 
 void Fight(int pI, int cI) {
-    if (pI == 1) {
-        if (cI == 0) {
-            printf("\nDRAW");
-        }
-        else if (cI == 1) {
-            printf("\nYOU LOSE.");
-        }
-        else {
-            printf("\nYOU WIN!");
-        }
+    if (pI == cI) {
+        printf("\nDRAW");
     }
-    if (pI == 2) {
-        if (cI == 0) {
-            printf("\nYOU WIN!");
-        }
-        else if (cI == 1) {
-            printf("\nDRAW");
-        }
-        else {
-            printf("\nYOU LOSE.");
-        }
+    else if ((pI == 1 && cI == 3) || (pI == 2 && cI == 1) || (pI == 3 && cI == 2)) {
+        printf("\nYOU WIN!");
     }
-    if (pI == 3) {
-        if (cI == 0) {
-            printf("\nYOU LOSE");
-        }
-        else if (cI == 1) {
-            printf("\nYOU WIN!");
-        }
-        else {
-            printf("\nDRAW");
-        }
+    else {
+        printf("\nYOU LOSE.");
     }
 }
